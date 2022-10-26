e1ac524fc886e1f8712dfdba7c4f30e38d79338d
YavuzKarakoc <yavuzkarakocsosyal@gmail.com>
2022-10-26 22:31:28 +0300

    add register page fluent validator

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriteName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şire kısmı boş geçilemez");
+            RuleFor(x => x.WriteName).MinimumLength(2).WithMessage("İsim Soyisim en az 2 karakter içermeli");
+            RuleFor(x => x.WriteName).MaximumLength(50).WithMessage("İsim Soyisim 50 karakterden fazla olamaz");
+        }
+    }
+}
--- CoreBlog/Controllers/RegisterController.cs
+++ CoreBlog/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreBlog.Controllers
@@ -18,10 +20,23 @@ namespace CoreBlog.Controllers
 		[HttpPost]
 		public IActionResult Index(Writer p)
 		{
-			p.WriterStatus = true;
-			p.WriterAbout = "değişecek burası";
-			wm.WriterAdd(p);
-            return RedirectToAction("Index", "Blog");
+			WriterValidator wv = new WriterValidator();
+			ValidationResult result = wv.Validate(p);
+			if(result.IsValid)
+			{
+                p.WriterStatus = true;
+                p.WriterAbout = "değişecek burası";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+			}
+			else
+			{
+				foreach(var item in result.Errors)
+				{
+					ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+				}
+			}
+			return View();
         }
 	}
 }
