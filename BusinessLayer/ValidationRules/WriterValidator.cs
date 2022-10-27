452e6eaaaa1bce86e476e6800fe436c53dade530
YavuzKarakoc <yavuzkarakocsosyal@gmail.com>
2022-10-27 16:15:55 +0300

    add commet action

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -12,11 +12,11 @@ namespace BusinessLayer.ValidationRules
     {
         public WriterValidator()
         {
-            RuleFor(x => x.WriteName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı soyadı kısmı boş geçilemez");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şire kısmı boş geçilemez");
-            RuleFor(x => x.WriteName).MinimumLength(2).WithMessage("İsim Soyisim en az 2 karakter içermeli");
-            RuleFor(x => x.WriteName).MaximumLength(50).WithMessage("İsim Soyisim 50 karakterden fazla olamaz");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("İsim Soyisim en az 2 karakter içermeli");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("İsim Soyisim 50 karakterden fazla olamaz");
         }
     }
 }
--- CoreBlog/Controllers/CommentController.cs
+++ CoreBlog/Controllers/CommentController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
@@ -13,13 +14,22 @@ namespace CoreBlog.Controllers
 		{
 			return View();
 		}
-		
+		[HttpGet]
 		public PartialViewResult PartialAddComment()
 		{
 			return PartialView();
 		}
+        [HttpPost]
+        public PartialViewResult PartialAddComment(Comment p)
+        {
+			p.CommentDate=DateTime.Parse(DateTime.Now.ToShortDateString());
+			p.CommentStatus = true;
+			p.BlogID = 7;
+			cm.CommentAdd(p);
+            return PartialView();
+        }
 
-		public PartialViewResult CommentListByBlog(int id)
+        public PartialViewResult CommentListByBlog(int id)
 		{
 			var values = cm.GetList(id);
 			return PartialView(values);
